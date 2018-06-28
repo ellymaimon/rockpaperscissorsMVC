1aa4c6b18427acc6e5994d11bb0df8d343a90bd3
Elly Maimon and Kevin Ahn <elly@>
2018-06-28 14:09:36 -0700

    add logic and test for getting the outcome, wire up main method

--- RockPaperScissors.Tests/ModelTests/Game.Tests.cs
+++ RockPaperScissors.Tests/ModelTests/Game.Tests.cs
@@ -9,9 +9,33 @@ namespace RockPaperScissors.Tests
     public class GameTest
     {
         [TestMethod]
-        public void Name()
+        public void GetSetPlayerOne_GetsSetsPlayerOne_True()
         {
+            Game testGame = new Game();
+            string test = "thaddius";
+            testGame.SetPlayerOne(test);
+            Assert.AreEqual(test, testGame.GetPlayerOne());
+        }
 
+        [TestMethod]
+        public void GetSetPlayerTwo_GetsSetsPlayerTwo_True()
+        {
+            Game testGame = new Game();
+            string test = "thadislame";
+            testGame.SetPlayerTwo(test);
+            Assert.AreEqual(test, testGame.GetPlayerTwo());
+        }
+
+        [TestMethod]
+        public void GetOutcome_GetsOutCome_True()
+        {
+            Game testGame = new Game();
+            string test1 = "rock";
+            string test2 = "paper";
+            string winMessage = "Player Two Wins";
+            testGame.SetPlayerOne(test1);
+            testGame.SetPlayerTwo(test2);
+            Assert.AreEqual(winMessage, testGame.GetOutcome());
         }
     }
 }
